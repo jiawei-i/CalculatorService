5898c06115d3dc7be33417d3dfdb95881a5addf0
ljw <414171762@qq.com>
2017-09-25 14:31:09 +0800

    增加悬浮框权限判断。

--- app/src/main/java/com/example/apple/calculatorservice/MainActivity.java
+++ app/src/main/java/com/example/apple/calculatorservice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.apple.calculatorservice;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -14,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.open_calculator_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CalculatorService.class);
-                startService(intent);
+                if (SystemUtil.getAppOps(MainActivity.this)) {
+                    Intent intent = new Intent(MainActivity.this, CalculatorService.class);
+                    startService(intent);
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, 10);
+                }
+
+
             }
         });
     }
--- app/src/main/java/com/example/apple/calculatorservice/SystemUtil.java
+++ app/src/main/java/com/example/apple/calculatorservice/SystemUtil.java
@@ -2,6 +2,8 @@ package com.example.apple.calculatorservice;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
@@ -74,5 +76,43 @@ class SystemUtil {
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
         return dp * (metrics.densityDpi / 160f);
+    }
+
+    /**
+     * 判断 悬浮窗口权限是否打开
+     *
+     * @param context
+     * @return true 允许  false禁止
+     */
+    public static boolean getAppOps(Context context) {
+       /* try {
+            Object object = context.getSystemService("appops");
+            if (object == null) {
+                return false;
+            }
+            Class localClass = object.getClass();
+            Class[] arrayOfClass = new Class[3];
+            arrayOfClass[0] = Integer.TYPE;
+            arrayOfClass[1] = Integer.TYPE;
+            arrayOfClass[2] = String.class;
+            Method method = localClass.getMethod("checkOp", arrayOfClass);
+            if (method == null) {
+                return false;
+            }
+            Object[] arrayOfObject1 = new Object[3];
+            arrayOfObject1[0] = Integer.valueOf(24);
+            arrayOfObject1[1] = Integer.valueOf(Binder.getCallingUid());
+            arrayOfObject1[2] = context.getPackageName();
+            int m = ((Integer) method.invoke(object, arrayOfObject1)).intValue();
+            return m == AppOpsManager.MODE_ALLOWED;
+        } catch (Exception ex) {
+
+        }
+        return false;*/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return Settings.canDrawOverlays(context);
+        } else {
+            return true;
+        }
     }
 }
